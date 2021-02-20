a91a0177a7f3945b60d92ebdb1df7aabd86143a3
yuukiasuna00 <yuukixasuna00@gmail.com>
2021-02-20 20:24:10 -0300

    feat: Add feature 'addTodo' to get newTodos and set onSubmit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,10 +4,20 @@ import TodoForm from './TodoForm';
 function TodoList() {
   const [todos, setTodos] = useState([]);
 
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+  };
+
   return (
     <div>
       <h1>What's the Plan for Today?</h1>
-      <TodoForm />
+      <TodoForm onSubmit={addTodo} />
     </div>
   );
 }
